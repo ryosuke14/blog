66c63ec8850c159cdbc816d2960df036f7213bfb
ユーザー名 <idakoutuu0205@gmail.com>
2020-09-28 22:23:45 +0900

    チェックブレード修正

--- resources/views/boards/check.blade.php
+++ resources/views/boards/check.blade.php
@@ -17,24 +17,24 @@
                 <form action="/board/create" method ="post" enctype="multipart/form-data">
                     @csrf
                     <div class="form-group col-md-6 row">
-                        <label>タイトル</label>
+                        <label>タイトル:</label>
                         {{ $inputs['title'] }}
                         <input class="form-control" type="hidden" name="title" >
                     </div>
                     <div class="form-group col-md-6 row">
-                        <label>本文</label>
+                        <label>本文:</label>
                         {{ $inputs['text'] }}
                         <input class="form-control" type="hidden" name="text" >
                     </div>
                     <div class="form-group col-md-6 row">
-                        <label>タグ</label>
+                        <label>タグ:</label>
                         {{ implode(',', $inputs['tag']) }}
                         @foreach ($inputs['tag'] as $tag_id => $tg)
                         <input class="form-control" type="hidden" name="tg[]" value="{{ $tag_id }}">
                         @endforeach
                     </div>
                     <div class="form-group col-md-6 row">
-                        <label>写真</label>
+                        <label>写真:</label>
                         <img src=" {{ url($uploadedFile) }}" alt="" width="100px" height="100px">
                         <input class="form-control" type="hidden" name="photo" value="{{ $uploadedFile }}">
                     </div>
