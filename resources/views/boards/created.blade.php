b547db096bb38a647daa9594fc090889fa7936d6
ユーザー名 <idakoutuu0205@gmail.com>
2020-09-29 23:24:24 +0900

    投稿完了画面、中間テーブル作成

---
+++ resources/views/boards/created.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.base')
+
+@section('title', '投稿完了')
+
+@section('content')
+<style>
+    .complete {
+        text-align: center;
+    }
+</style>
+<div class="complete">
+    <h2 class="">投稿完了</h2>
+    <p>トップへ</p>
+</div>
+@endsection
