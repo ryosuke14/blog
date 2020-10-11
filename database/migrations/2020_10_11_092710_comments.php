b24c7afe38c9cf27c918c18845f7e605ff6f9914
yazawaryosuke <ykdyd2kut@yahoo.co.jp>
2020-10-11 18:28:41 +0900

    commentテーブル作成

---
+++ database/migrations/2020_10_11_092710_comments.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Comments extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('comment_name');
+            $table->string('comment_text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
