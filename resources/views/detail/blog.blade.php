b74af8dd2c3dbb261d6eab68955e386a10adc11f
yazawaryosuke <ykdyd2kut@yahoo.co.jp>
2020-10-28 01:57:30 +0900

    no message

--- resources/views/detail/blog.blade.php
+++ resources/views/detail/blog.blade.php
@@ -72,6 +72,17 @@
             <h5 class="mt-0">Commenter Name</h5>
             Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
         </div>
+
+        <div class="media mb-4">
+        @foreach($comments as $comment)
+          <div class="media-body">
+            <h5 class="mt-0">{{ $comment->comment_name }}</h5>
+            {{ $comment->comment_text }}
+          </div>
+        @endforeach
+        </div>
+
+
         
     </div>
 
